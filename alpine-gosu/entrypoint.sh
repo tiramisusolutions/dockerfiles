40afeaa5abdc340b1ed225fd16b029f0441d014a
sven <sven@so36.net>
2016-06-07 10:38:57 +0200

    Little updates and fixes

--- alpine-gosu/entrypoint.sh
+++ alpine-gosu/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Add local user
+# Add local user, named cu -> short for containeruser
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
 
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-adduser -D -s /bin/bash -u $USER_ID -o -c "" -m containeruser
-export HOME=/home/containeruser
+adduser -s /bin/bash -D  cu
+export HOME=/home/cu
 
-exec gosu wtd "$@"
+exec /usr/local/bin/gosu cu "$@"
