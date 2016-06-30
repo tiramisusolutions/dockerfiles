3a757c22ea90974cbb0d22fd91f0969e87b7ae4f
sven <sven@so36.net>
2016-06-30 08:55:17 +0200

    Remove: mr.ci

--- mr.ci/README.md
+++
@@ -1,7 +0,0 @@
-
-
-
-docker run -it --rm -v ${PWD}/:/build/app:rw -v ${PWD}/docs:/build/docs -u $(id -u):$(id -g) --name ci-test quay.io/tiramisu/mr.ci
-
-Loop through dir and check all files:
-
--- mr.ci/check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd app
-echo "Running Linkcheck"
-../tests.sh | while read -r line ; do sphinx-build -c../conf -b linkcheck -d docs/_build/doctrees docs docs/_build/tests/$line $line; done
